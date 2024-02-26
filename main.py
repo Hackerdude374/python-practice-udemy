4f00ba1f962172a193376a025669333da2088aba
Robert Le <Robert.Le@CIX.CSI.CUNY.EDU>
2024-02-26 12:42:40 -0500

    string slicing

--- main.py
+++ main.py
@@ -17,6 +17,39 @@ print(sarah)
 print(bob)
 print(mike)
 
+
 sarah = bob = mike
 
 print (sarah, bob, mike)
+
+#arithmetic operators and strings
+
+age1 = 12
+age2 = 18
+
+print(age1 + age2) # should be 30
+
+print (age2 - age1) 
+
+print (age2*age1)
+
+print (age2 /age1)
+print ("test")
+#-------------------------------------
+#strings 
+
+sent1 = "today is a beautiful day"
+
+print(sent1)
+
+first = "joe"
+last = "he"
+
+print (first + last)
+print (first + " " + last)
+
+print (first * 10)
+
+#slicing (each character has an index)
+sent = "bozo"
+print (sent[0])
