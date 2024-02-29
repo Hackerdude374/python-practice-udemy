31484973bf33f9762e6b31f9bed2e7c6e590b38f
Hackerdude374 <robertle982@gmail.com>
2024-02-29 12:53:25 -0500

    finished hw

---
+++ hw.py
@@ -0,0 +1,63 @@
+# 1. Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+print("Q1")
+num1 = 15
+num2 = 30 
+print ((num1+num2) /2)
+
+
+# 2. Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder.
+print("Q2")
+num1 = 2 
+num2 = 4
+
+print (num1+num2)
+print(num1 - num2)
+print( num1 / num2)
+print (num1 *num2)
+print (num1 % num2)
+
+# 3. Create a variable called name and store your name in it as a string.
+
+name = "Joe he"
+print(name)
+
+# 4. Create three variables in one line and assign each one a different food item.
+
+Joe, Mike, Rob = "pizza", "soda", "fries"
+print(Joe, Mike, Rob)
+
+# 5. Print out "Hello" ten times using arithmetic operators.
+
+print (("hello ") *10)
+
+# 6. Set your name and age variables in one line with multiple assignment
+print ("q6")
+name, age = "Robert Le", 20
+print(name, age)
+
+# 7. Create two strings and then create a third variable combining both of the two original strings.
+print("q7")
+fname = "joe"
+lname = "he"
+print(fname + lname)
+
+
+# 8. Create a string and print the fourth letter of the word.
+print ("q8")
+name = "joe"
+print(name[0])
+
+# 9. Create a string and print the letters from index 0 to 5.
+
+print ("q9") 
+name = "MAX PAYNE"
+print(name[0:5])
+
+
+
+# 10. Create a variable and print all the letters from the first index until the end.
+
+print ("q10")
+name = "JOEHEHEHEHEHHE"
+print(name[0:])
+
